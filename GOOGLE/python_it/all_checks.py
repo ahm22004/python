78da53842189e1de1f944af7091cda3f811b3b60
ahm22004 <ahm22004@gmail.com>
2022-04-17 17:25:00 +0200

    Complete reboot func & initaite check_disk func

--- GOOGLE/python_it/all_checks.py
+++ GOOGLE/python_it/all_checks.py
@@ -1,10 +1,17 @@
 #!/user/bin/env python3
 
+from ast import Return
+from cProfile import run
 import os
 import sys
 
 
 def check_reboot():
+    '''Returns True if the computer has a pending reboot.'''
+    return os.path.exists('/run/reboot_required')
+
+
+def check_disk_full(disk, min_absolute, min_percent):
     pass
 
 
