c65b1f14218addf569d53de3a5fb75f0e53c3ffb
ahm22004 <ahm22004@gmail.com>
2022-05-18 21:25:51 +0200

    Adding new fancy scripts

---
+++ GOOGLE/python_it/network.py
@@ -0,0 +1,15 @@
+#!/user/bin/env python3
+import requests
+import socket
+
+
+def check_localhost():
+    localhost = socket.gethostbyname('localhost')
+    if localhost == '127.0.0.1':
+        return True
+
+
+def check_connectivity():
+    request = requests.get("http://www.google.com")
+    if request == 200:
+        return True
